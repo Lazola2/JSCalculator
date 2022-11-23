f49f885d72d75ca19cb545c74730148bca6c52c8
Lazola2 <lazolamakubalo271@gmail.com>
2022-11-23 11:49:25 +0200

    fixed minor error on alignment

--- js/code.js
+++ js/code.js
@@ -1,13 +1,11 @@
 // inputs
 let buttons = document.querySelectorAll(".buttons");
-let operators = document.querySelectorAll(".operators");
 let screen = document.querySelector("#screen");
 let equal = document.querySelector(".equal");
 let clearBtn = document.querySelector(".clear");
 let deleteBtn = document.querySelector(".btn-delete");
 let btnZero = document.querySelector(".zero");
 let btnComma = document.querySelector(".btn-comma");
-
 
 // processing for all the buttons 1 to 9 including the operators ( - , + , / , *)
 // assume that the is no text in the screen
@@ -50,35 +48,7 @@ deleteBtn.addEventListener('click', () => {
 
 // comma button
 btnComma.addEventListener('click', () => {
-    // if (!screen.value.split('').includes('.')) {
-    //     screen.value += btnComma.value;
-    // }
-
-    if (validateDecimals(screen.value)) {
-        alert('Still good');
+    if (!screen.value.split('').includes('.')){
+        screen.value += btnComma.value;
     }
-    else{
-        alert('Not good');
-    }
-});
-
-function validateDecimals(value) {
-    // value1 : '12.34.44 + 23.43.55' // false
-    // value2 : '12..44 + 23.55' // false
-    // value3 : '12.34 + 23.55' // true
-    let countCommas = 0;
-    let vFlag = true;
-
-    // ['1','2','.','3','+','1','.','5']
-    value.split('').forEach(val => {
-        if (val === '.') countCommas++;
-        if (!((['-', '+', '/', '*'].includes(val)) && (countCommas < 2))) 
-            vFlag = false;
-    })
-    return vFlag;
-    // value.split('').forEach( val => {
-
-    // })
-}
-
-
+})
