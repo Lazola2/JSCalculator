3b6bfb53d39b10e5e0de43c160b69356d7fc5d6a
Lazola2 <lazolamakubalo271@gmail.com>
2022-11-25 12:23:13 +0200

    Fixed a bug about the decimal

--- js/code.js
+++ js/code.js
@@ -9,7 +9,11 @@ let btnZero = document.querySelector(".zero");
 let btnComma = document.querySelector(".btn-comma");
 
 // this array is used to validate if the is a comma on the screen
-let arr = []; 
+let arr = [];
+
+// this variable will be the memory for when the user clicks delete
+// it will undo the clearing of the array used to validate comma
+let memory = [];
 
 // processing for all the buttons 1 to 9 including the operators ( - , + , / , *)
 // assume that the is no text in the screen
@@ -32,6 +36,7 @@ buttons.forEach(button => {
 // the onclick event of each operator should clear the array used for validating commas
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
+        memory = [...arr];
         arr = [];
     })
 })
@@ -39,13 +44,14 @@ operators.forEach(operator => {
 // equal button
 equal.addEventListener('click', () => {
     try {
-        if ( !eval(screen.value).toString().includes('.')) {
+        if (!eval(screen.value).toString().includes('.')) {
             screen.value = eval(screen.value);
-            arr = [];            
+            arr = [];
             return;
         }
         screen.value = eval(screen.value).toFixed(2);
-        arr = [...screen.value.split('')];
+        [memory,arr] = [...screen.value.split('')];
+        
         noText = !noText;
     }
     catch (e) {
@@ -60,19 +66,26 @@ equal.addEventListener('click', () => {
 const clearScreen = () => {
     screen.value = "";
     arr = [];
+    memory = [];
 }
 
 clearBtn.addEventListener('click', clearScreen);
 
 // delete button
 deleteBtn.addEventListener('click', () => {
+    if (memory.includes('.')) {
+        console.log('the memory has a comma');
+        console.log(`memory: [${memory}]`)
+    }
     screen.value = screen.value.slice('0', '-1');
+    memory.pop();
     arr.pop();
+    memory = [...screen.value.split('')]
 });
 
 // comma button
 btnComma.addEventListener('click', () => {
-    if (!arr.includes('.')) {
+    if (!arr.includes('.') && !memory.includes('.')) {
         screen.value += btnComma.value;
         arr.push(btnComma.value);
     }
